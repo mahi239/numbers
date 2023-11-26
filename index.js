385c1e523ec34bb7fc118472ebeb6cead055ee84
mahi239 <148883188+mahi239@users.noreply.github.com>
2023-11-26 09:55:53 +0000

    second commit

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ console.log(nestedArray[2][1][0]);
 
 /* Random Numbers 1 to 50  by function deacleration */
 function oneto50() {
-  const RandomNumber = Math.floor(Math.random() * 51);
-  return RandomNumber;
+  const randomandomNumber = Math.floor(Math.random() * 51);
+  return randomNumber;
 }
 console.log(oneto50());
 console.log(oneto50());
@@ -17,8 +17,8 @@ console.log(oneto50());
 
 /* Random numbers 1 to 100 by Function expression */
 const oneto100 = function () {
-  const RandomNumber = Math.floor(Math.random() * 100) + 1;
-  return RandomNumber;
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  return randomNumber;
 };
 console.log(oneto100());
 console.log(oneto100());
